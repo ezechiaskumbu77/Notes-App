cd4038870f956b9db46b39043674b152b4b2258f
ezechiaskumbu <ezechiaskumbu77@gmail.com>
2021-06-29 10:59:52 +0100

    Add,Remove, Savce and load notes working

---
+++ app.js
@@ -0,0 +1,60 @@
+
+const chalk = require('chalk')
+// const validator = require('validator')
+const yargs = require('yargs')
+const notes = require('./notes.js')
+
+//customize yargs version 
+yargs.version('16.06.2021')
+
+//create add command
+yargs.command({
+    command: 'add',
+    description : 'Add new note',
+    builder:{
+        title:{
+            description:'Note title',
+            demandOption:true,
+            type:'string'
+        }
+    },
+    handler(argv){
+        notes.addNote(argv.title, argv.body)
+    }
+})
+
+
+//Create remove command
+yargs.command({
+    command: 'remove',
+    description : 'Remove old note',
+    builder:{
+        title:{
+            description:'Note title',
+            demandOption:true,
+            type:'string'
+        }
+    },
+    handler(argv){
+        notes.removeNote(argv.title, argv.body)
+    }
+})
+
+//List command
+yargs.command({
+    command: 'list',
+    description : 'List the note',
+    handler(argv){
+        console.log("Listing the note!")
+    }
+})
+
+//Read command
+yargs.command({
+    command: 'read',
+    description : 'Read the note',
+    handler(argv){
+        console.log("Reading the note!")
+    }
+})
+yargs.parse()
---
+++ notes.js
@@ -0,0 +1,62 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+const getNotes = () => {
+    return 'Your notes....'
+}
+/** ******************* Add new notes **************** */  
+const addNote = (title,body) => {
+  const notes = loadNotes()
+  const duplicateNotes = notes.filter((note) => note.title === title)
+  if(duplicateNotes.length === 0){
+    notes.push({
+        title: title,
+        body: body
+    })
+    saveNotes(notes)
+    console.log(chalk.green.inverse('New note added! '))
+  }else{
+    console.log(chalk.red.inverse('Note title taken!'))
+  }
+  
+}
+/** ******************* End of Add new notes **************** */ 
+
+
+/** ******************* Remove old notes ******************* */ 
+const removeNote = (title) => {
+  const notes = loadNotes()
+  const notesToKeep = notes.filter((note) => note.title !== title)
+  if (notes.length > notesToKeep.length){
+      console.log(chalk.green.inverse('Note removed!'))
+      saveNotes(notesToKeep)
+  }else{
+    console.log(chalk.red.inverse('Note not found!'))
+  }
+}
+/** ******************* End of Remove old notes **************** */  
+
+/** ***************************** saveNote  ******************* */ 
+const saveNotes = (notes) => {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+/** *************************** End of saveNote  ************* */ 
+
+/** ***************************** Load Notes  *************** */ 
+const loadNotes = () => {
+   try{
+    const dataBuffer = fs.readFileSync('notes.json')
+    const dataJSON = dataBuffer.toString()
+    return JSON.parse(dataJSON)
+   } catch(e){
+       return []
+   }
+} 
+/** ********************* End of Load Notes  *************** */ 
+
+module.exports = {
+    getNotes : getNotes,
+    addNote : addNote,
+    removeNote : removeNote
+}
